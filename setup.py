1899527b609e0e32acec02f8f5efdd5925b03c41
Vishal Tala <vishaltala@Vishals-MacBook-Pro.local>
2024-04-30 15:27:16 +0200

    Setup completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'ObjectDetection',
+    version = '0.0.0',
+    author = 'Vishal Tala',
+    author_email = 'talavishal87@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
