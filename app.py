62caf4f515cea21d433c8dc7353d3684181de217
vishaltala <talavishal87@gmail.com>
2024-05-03 11:50:42 +0200

    small change

--- app.py
+++ app.py
@@ -36,9 +36,9 @@ def predictRoute():
 
         os.system("cd yolov5/ && python detect.py --weights my_model.pt --img 416 --conf 0.5 --source ../data/inputImage.jpg")
 
-        opencodedbase64 = encodeImageIntoBase64("yolov5/runs/detect/exp/inputImage.jpg")
+        opencodedbase64 = encodeImageIntoBase64("yolov5/runs/inputImage.jpg")
         result = {"image": opencodedbase64.decode('utf-8')}
-        os.system("rm -rf yolov5/runs")
+        #os.system("rm -rf yolov5/runs")
 
     except ValueError as val:
         print(val)
